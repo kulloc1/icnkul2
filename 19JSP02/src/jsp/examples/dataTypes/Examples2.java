bb3c13bc9dea27d314b2ff2882d50ffa0ac17100
kulloc1 <139840140+kulloc1@users.noreply.github.com>
2025-01-02 09:21:34 +0700

    Update Examples2.java
    fs213

--- 19JSP02/src/jsp/examples/dataTypes/Examples2.java
+++ 19JSP02/src/jsp/examples/dataTypes/Examples2.java
@@ -16,7 +16,7 @@ public class Examples2 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner input1 = new Scanner(System.in);
+		Scanner input1 123= new Scanner(System.in);
 		System.out.println("Please enter your name : ");
 		String fullName =  input1.nextLine();
 		System.out.println("Your fullName is : " + fullName);
